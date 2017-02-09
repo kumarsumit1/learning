9ffe10ab97f1ba6a353f16bb5bf166e3b714fe51
Sourav Gulati <sgulati@ap.equinix.com>
2017-02-09 14:18:23 +0800

    Added LFS Example

--- src/main/java/com/packt/sfjd/ch5/LFSExample.java
+++ src/main/java/com/packt/sfjd/ch5/LFSExample.java
@@ -3,7 +3,6 @@ package com.packt.sfjd.ch5;
 import java.util.Arrays;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
@@ -15,15 +14,27 @@ public class LFSExample {
 		SparkConf conf =new SparkConf().setMaster("local").setAppName("Local File System Example");
 		JavaSparkContext jsc=new JavaSparkContext(conf);
 		
-		JavaRDD<String> localFile = jsc.textFile("C:\\Users\\sgulati\\Documents\\Result\\Missing_Alarms1.txt");
+		JavaRDD<String> localFile=jsc.textFile("C:\\Users\\sgulati\\Documents\\Result\\test\\a.txt");
 		localFile.flatMap(x -> Arrays.asList(x.split(" ")).iterator()).mapToPair(x -> new Tuple2<String, Integer>((String) x, 1))
-		.reduceByKey((x, y) -> x + y);
-//		JavaPairRDD<String, Integer> wordCountLocalFile =localFile.flatMap(x -> Arrays.asList(x.split(" ")).iterator())
-//			.mapToPair(x -> new Tuple2<String, Integer>((String) x, 1))
-//			.reduceByKey((x, y) -> x + y);
+		.reduceByKey((x, y) -> x + y).saveAsTextFile("C:\\Users\\sgulati\\Documents\\Result\\out_path");
 		
-		//System.out.println(wordCountLocalFile.collect());
 		
+		JavaRDD<String> localFile1 = jsc.textFile("C:\\Users\\sgulati\\Documents\\Result\\test\\a.txt,C:\\Users\\sgulati\\Documents\\Result\\test\\.txt");
+		
+		localFile1.flatMap(x -> Arrays.asList(x.split(" ")).iterator()).mapToPair(x -> new Tuple2<String, Integer>((String) x, 1))
+		.reduceByKey((x, y) -> x + y).saveAsTextFile("C:\\Users\\sgulati\\Documents\\Result\\out_path1");
+		
+		JavaRDD<String> localFile2 =jsc.textFile("C:\\Users\\sgulati\\Documents\\Result\\test\\*");
+		
+		localFile2.flatMap(x -> Arrays.asList(x.split(" ")).iterator()).mapToPair(x -> new Tuple2<String, Integer>((String) x, 1))
+		.reduceByKey((x, y) -> x + y).saveAsTextFile("C:\\Users\\sgulati\\Documents\\Result\\out_path2");
+	   
+        JavaRDD<String> localFile3 =jsc.textFile("C:\\Users\\sgulati\\Documents\\Result\\test\\*,C:\\Users\\sgulati\\Documents\\Result\\test5\\*");
+		
+        localFile3.flatMap(x -> Arrays.asList(x.split(" ")).iterator()).mapToPair(x -> new Tuple2<String, Integer>((String) x, 1))
+		.reduceByKey((x, y) -> x + y).saveAsTextFile("C:\\Users\\sgulati\\Documents\\Result\\out_path3");
+		
+		jsc.close();
 		
 	}
 }
